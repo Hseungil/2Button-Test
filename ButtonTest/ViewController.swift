cc3c03d0ed085023fb14d9d831b3d5f43cc0c2c1
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-13 16:13:15 +0900

    2nd commit

--- ButtonTest/ViewController.swift
+++ ButtonTest/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
     }
 
-
+    @IBAction func leftBunPressed(_ sender: Any) {
+        myLabel.text = "Left Button Pressed!"
+    }
+    
+    @IBAction func rightBtnPressed(_ sender: Any) {
+        myLabel.text = "Right Button Pressed!"
+    }
 }
 
